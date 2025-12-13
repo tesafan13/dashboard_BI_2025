0fb9883857c23eb5cb9de02422a927cba62a5e5a
tesafan13 <teresasofiagonzalez@gmail.com>
2025-12-13 17:07:10 -0600

    Improve data handling and plotting for ADR
    Refactor data processing and visualization for hotel ADR analysis.

--- main.py
+++ main.py
@@ -31,68 +31,68 @@ with tab1:
         st.caption("Nota: Primeros 30 valores del archivo CSV")
         st.write("""En las columnas se pueden observar datos como el tipo de hotel, reservaciones, cancelaciones, fechas y tiempos de llegada, permisos, el páis de origen, tipo de cuarto, forma de pago, y otros datos. 
         """)
-    df.dropna(subset=['children'], inplace=True)
-    df.dropna(subset=['country'], inplace=True)
-    df['arrival_date'] = df['arrival_date_year'].astype(str) + '-' + df['arrival_date_month'] + '-' + df['arrival_date_day_of_month'].astype(str)
-    cols = list(df.columns)
-    cols.insert(3, cols.pop(cols.index('arrival_date')))
-    df = df.loc[:, cols]
-    df.drop(['arrival_date_year', 'arrival_date_month', 'arrival_date_day_of_month'], axis=1, inplace=True)
-    df['arrival_date'] = pd.to_datetime(df['arrival_date'])
-    df['reservation_status_date'] = pd.to_datetime(df['reservation_status_date'])
-    df['is_canceled'] = df['is_canceled'].astype(bool)
-    df['is_repeated_guest'] = df['is_repeated_guest'].astype(bool)
-    df['children'] = df['children'].astype(int)
-    df[['agent', 'company']].dropna(subset=['agent', 'company']).head(20)
-    df.drop(["is_canceled",'booking_changes', 'reservation_status', 'reservation_status_date', 'required_car_parking_spaces', 'total_of_special_requests', "assigned_room_type", "agent", "company"], axis=1, inplace=True)
-    dummy = df.drop(columns=['country'])
-    dummy=pd.get_dummies(dummy, dtype=int)
-    df.drop(df[df['adr'] == 5400].index, inplace=True)
-    df.drop(df[df['adr'] < 0].index, inplace=True)
-    df[df['adults'] > 10]
-    df[df['children'] >= 3].sort_values('children', ascending=False)
-    df.drop(df[df['children'] == 10].index, inplace=True)
-    df.drop(df[df['babies'] >= 3].index, inplace=True)
-    df[((df['adults'] == 0) & (df['children'] > 0)) | ((df['adults'] == 0) & (df['babies'] > 0))]
-    df = df[(df['adults'] > 0)]
-    df[df['days_in_waiting_list'] > 0].sort_values('days_in_waiting_list', ascending=False)
-    df[df['days_in_waiting_list'] > 0]['days_in_waiting_list'].count()
-    months = ['January', 'February', 'March', 'April', 'May', 'June',
-             'July', 'August', 'September', 'October', 'November', 'December']
-    df = df.drop(['arrival_date'], axis=1)
-    line_data = df.groupby([
-    df['arrival_date'].dt.month_name(),
-    'hotel'
-    ])['adr'].mean().reset_index()
+        df.dropna(subset=['children'], inplace=True)
+        df.dropna(subset=['country'], inplace=True)
+        df['arrival_date'] = df['arrival_date_year'].astype(str) + '-' + df['arrival_date_month'] + '-' + df['arrival_date_day_of_month'].astype(str)
+        cols = list(df.columns)
+        cols.insert(3, cols.pop(cols.index('arrival_date')))
+        df = df.loc[:, cols]
+        df.drop(['arrival_date_year', 'arrival_date_month', 'arrival_date_day_of_month'], axis=1, inplace=True)
+        df['arrival_date'] = pd.to_datetime(df['arrival_date'])
+        df['reservation_status_date'] = pd.to_datetime(df['reservation_status_date'])
+        df['is_canceled'] = df['is_canceled'].astype(bool)
+        df['is_repeated_guest'] = df['is_repeated_guest'].astype(bool)
+        df['children'] = df['children'].astype(int)
+        df[['agent', 'company']].dropna(subset=['agent', 'company']).head(20)
+        df.drop(["is_canceled",'booking_changes', 'reservation_status', 'reservation_status_date', 'required_car_parking_spaces', 'total_of_special_requests', "assigned_room_type", "agent", "company"], axis=1, inplace=True)
+        dummy = df.drop(columns=['country'])
+        dummy=pd.get_dummies(dummy, dtype=int)
+        df.drop(df[df['adr'] == 5400].index, inplace=True)
+        df.drop(df[df['adr'] < 0].index, inplace=True)
+        df[df['adults'] > 10]
+        df[df['children'] >= 3].sort_values('children', ascending=False)
+        df.drop(df[df['children'] == 10].index, inplace=True)
+        df.drop(df[df['babies'] >= 3].index, inplace=True)
+        df[((df['adults'] == 0) & (df['children'] > 0)) | ((df['adults'] == 0) & (df['babies'] > 0))]
+        df = df[(df['adults'] > 0)]
+        df[df['days_in_waiting_list'] > 0].sort_values('days_in_waiting_list', ascending=False)
+        df[df['days_in_waiting_list'] > 0]['days_in_waiting_list'].count()
+        months = ['January', 'February', 'March', 'April', 'May', 'June',
+                 'July', 'August', 'September', 'October', 'November', 'December']
+        df = df.drop(['arrival_date'], axis=1)
+        line_data = df.groupby([
+        df['arrival_date'].dt.month_name(),
+        'hotel'
+        ])['adr'].mean().reset_index()
+    
+        line_data['month'] = pd.Categorical(
+            line_data['arrival_date'],
+            categories=months,
+            ordered=True
+        )
 
-    line_data['month'] = pd.Categorical(
-        line_data['arrival_date'],
-        categories=months,
-        ordered=True
-    )
+        line_data = line_data.sort_values('month')
 
-    line_data = line_data.sort_values('month')
+        fig, ax1 = plt.subplots(figsize=(12, 8))
 
-    fig, ax1 = plt.subplots(figsize=(12, 8))
+        sns.lineplot(
+            line_data,
+            x='month',
+            y='adr',
+            hue='hotel',
+            marker='o',
+            ax=ax1
+        )
 
-    sns.lineplot(
-        line_data,
-        x='month',
-        y='adr',
-        hue='hotel',
-        marker='o',
-        ax=ax1
-    )
+        ax1.set_title("Tarifa diaria promedio por mes y tipo de hotel", fontsize=20, pad=25)
+        ax1.set_xlabel("Mes", fontsize=20, labelpad=18)
+        ax1.set_ylabel("Tarifa diaria promedio (ADR)", fontsize=16, labelpad=15)
+        ax1.tick_params(axis='x', rotation=18, labelsize=12)
+        ax1.tick_params(axis='y', labelsize=14)
+        ax1.legend(title="Tipo de hotel")
 
-    ax1.set_title("Tarifa diaria promedio por mes y tipo de hotel", fontsize=20, pad=25)
-    ax1.set_xlabel("Mes", fontsize=20, labelpad=18)
-    ax1.set_ylabel("Tarifa diaria promedio (ADR)", fontsize=16, labelpad=15)
-    ax1.tick_params(axis='x', rotation=18, labelsize=12)
-    ax1.tick_params(axis='y', labelsize=14)
-    ax1.legend(title="Tipo de hotel")
-
-    plt.tight_layout()
-    st.pyplot(fig)
+        plt.tight_layout()
+        st.pyplot(fig)
 
 with tab2:
         st.header("❌ Cancelaciones", divider = 'red')
